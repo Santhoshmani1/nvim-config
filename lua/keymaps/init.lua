903c3608517a168dc2cd5f95ebfd88e0f3480d33
Santhoshmani1 <pitakasanthosh@gmail.com>
2025-02-09 09:37:50 +0530

    fix: Modify Keyword and Number color highlighting

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -26,3 +26,12 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "g=", function()
+	local current_pos = vim.fn.getpos(".") -- Save current cursor position
+	vim.cmd("normal! ggVG=") -- Reindent the entire file
+	vim.fn.setpos(".", current_pos) -- Restore cursor position
+end, { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-t>", "<Cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-t>", [[<C-\><C-n><Cmd>ToggleTerm<CR>]], { noremap = true, silent = true })
